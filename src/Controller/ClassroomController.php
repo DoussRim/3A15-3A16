de206cab3cf7d0c7a30456955e8a7e0c4acd0847
DoussRim <rim.douss@esprit.tn>
2023-02-14 12:39:21 +0100

    Projet SMF

---
+++ src/Controller/ClassroomController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ClasseroomRepository;
+
+class ClassroomController extends AbstractController
+{
+    #[Route('/classroom', name: 'app_classroom')]
+    public function index(): Response
+    {
+        return $this->render('classroom/index.html.twig', [
+            'controller_name' => 'ClassroomController',
+        ]);
+    }
+    function Affiche(ClasseroomRepository $repo){
+        $classroom=$repo->findAll();
+        return $this->render('classroom/Affiche.html.twig',
+    ['cc'=>$classroom]);
+
+    }
+}
